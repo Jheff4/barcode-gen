be0a6cd16770bb0bdc8f03eb4893a8434dfefba6
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2023-02-08 14:05:49 +0100

    updated the JS

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const qr = document.getElementById('qrcode')
 const onGenerateSubmit = (e) => {
   e.preventDefault()
 
-  const url = document.getElementById('url');
-  
+  const url = document.getElementById('url')
+  const size = document.getElementById('size')
+
+  console.log(url, size)
 }
 
 form.addEventListener('submit', onGenerateSubmit)
