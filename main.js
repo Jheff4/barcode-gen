3f43213917d36800a918682df918dcc6c8ff386b
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2023-02-08 13:59:59 +0100

    updated the JS

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 const form = document.getElementById('form-generate')
 const qr = document.getElementById('qrcode')
+
+
+const onGenerateSubmit = (e) => {
+  
+}
+
+form.addEventListener('submit', onGenerateSubmit)
+
