cbc5c65837be4cdf81dfae0eddab58c87e64a74b
Jheff4 <75406244+Jheff4@users.noreply.github.com>
2023-02-10 19:12:21 +0100

    improved the spinner

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const form = document.getElementById('generate-form');
 const qr = document.getElementById('qrcode');
-
+const spinner = document.querySelector('#spin')
 
 const onGenerateSubmit = (e) => {
   e.preventDefault();
 
   const url = document.getElementById('url').value;
   const size = document.getElementById('size').value;
-  const spinner = document.getElementById('spin')
+
 
   if (url === '') {
     alert('Please, enter a URL');
@@ -18,17 +18,14 @@ const onGenerateSubmit = (e) => {
 };
 
 const showSpinner = () => {
-  const spin = document.createElement('div')
-  spin.classList.add('spinner')
-  
-  .style.display = "block";
+  document.getElementById('spin').style.display = "block";
 }
 
-const hideSpinner = () => {
-  document.getElementById('spinner').style.display = "none";
-}
+// const hideSpinner = () => {
+//   document.getElementById('spin').style.display = "none";
+// }
 
-hideSpinner();
+// hideSpinner();
 
 form.addEventListener('submit', onGenerateSubmit);
 
